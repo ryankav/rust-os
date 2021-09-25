2894637fc619a86a26c77c1531e9bc6f36f1d3a8
Ryan <ryan.p.kav@gmail.com>
2021-09-25 10:09:37 +0100

    removing the println! macro from std

---
+++ rust_os/target/debug/deps/rust_os-eb2abdfb3cbfe656.d
@@ -0,0 +1,5 @@
+/home/ryan/git-projects/rust-os/rust_os/target/debug/deps/rust_os-eb2abdfb3cbfe656: src/main.rs
+
+/home/ryan/git-projects/rust-os/rust_os/target/debug/deps/rust_os-eb2abdfb3cbfe656.d: src/main.rs
+
+src/main.rs:

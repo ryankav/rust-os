b1fd779402eb11c98afb7a17ab02203c1a35431f
Ryan Kavanagh <ryan.p.kav@gmail.com>
2022-10-28 22:26:18 +0100

    Changed repo layout to rust workspace

--- rust_os/target/debug/deps/rust_os-eb2abdfb3cbfe656.d
+++
@@ -1,5 +0,0 @@
-/home/ryan/git-projects/rust-os/rust_os/target/debug/deps/rust_os-eb2abdfb3cbfe656: src/main.rs
-
-/home/ryan/git-projects/rust-os/rust_os/target/debug/deps/rust_os-eb2abdfb3cbfe656.d: src/main.rs
-
-src/main.rs:

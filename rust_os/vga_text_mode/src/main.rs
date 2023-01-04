7e50587033332b380c9c49e4980907f2da7321b8
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-01-04 15:26:13 +0000

    Cp previous main & use print something

--- rust_os/vga_text_mode/src/main.rs
+++ rust_os/vga_text_mode/src/main.rs
@@ -1,2 +1,20 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
 mod vga_buffer;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {}
+}
 

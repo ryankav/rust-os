1587c5b73a8c92ef29e8d9f0bac2f75f1b8b22a0
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-01-06 12:57:26 +0000

    Using WRITER to write to buffer in _start

--- rust_os/vga_text_mode/src/vga_buffer.rs
+++ rust_os/vga_text_mode/src/vga_buffer.rs
@@ -130,17 +130,3 @@ impl fmt::Write for Writer {
     }
 }
 
-pub fn print_something() {
-    use core::fmt::Write;
-
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
-
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
-}
-

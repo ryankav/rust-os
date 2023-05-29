745a14bdb5444e9bcfc5f82dc0c91ee70f19c021
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-05-29 15:17:15 +0100

    Setup first test

---
+++ test_framework/src/main.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+  println!("Running {} tests", tests.len());
+  for test in tests {
+    test();
+  }
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  println!("{}", info);
+  loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+  println!("Hello, World{}", "!");
+
+  #[cfg(test)]
+  test_main();
+
+  loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+  print!("trivial assertion... ");
+  assert_eq!(1, 1);
+  println!("[ok]");
+}
---
+++ test_framework/src/vga_buffer.rs
@@ -0,0 +1,165 @@
+//! The vga code layout is two bytes with the following structure:
+//! the first byte is the ascii (really code page 437 but near enough) char to
+//! print. Then in the second byte the first four bits are the foreground color
+//! ie the char character, then the next three bits are the background color,
+//! and finally the remaining bit is used to set wether the char should blink.
+//! Hopefully the diagram below helps in visualising the above
+//!
+//!          VGA bit layout
+//!
+//!                      1
+//!  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5  
+//! *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+//! |   Ascii char  |  fg   |  bg |b|
+//! *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+//!
+//! fg: color = foreground, bg: color = background, b: bool = blink
+//!
+use core::fmt;
+use lazy_static::lazy_static;
+use spin::Mutex;
+use volatile::Volatile;
+
+lazy_static! {
+  pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+    column_position: 0,
+    color_code: ColorCode::new(Color::Yellow, Color::Black),
+    buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+  });
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+  Black = 0,
+  Blue = 1,
+  Green = 2,
+  Cyan = 3,
+  Red = 4,
+  Magenta = 5,
+  Brown = 6,
+  LightGray = 7,
+  DarkGray = 8,
+  LightBlue = 9,
+  LightGreen = 10,
+  LightCyan = 11,
+  LightRed = 12,
+  Pink = 13,
+  Yellow = 14,
+  White = 15,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+  fn new(foreground: Color, background: Color) -> ColorCode {
+    ColorCode((background as u8) << 4 | (foreground as u8))
+  }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+  ascii_character: u8,
+  color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+  // need the screen to be volatile as we don't read from it so, in theory, the
+  // compiler could optimise away the code to buffer as it looks like we don't
+  // use it. Volatile is basically a way for us to say that there are desired
+  // side effects from these writes
+  chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+  column_position: usize,
+  color_code: ColorCode,
+  buffer: &'static mut Buffer,
+}
+
+impl Writer {
+  pub fn write_byte(&mut self, byte: u8) {
+    match byte {
+      b'\n' => self.new_line(),
+      byte => {
+        if self.column_position >= BUFFER_WIDTH {
+          self.new_line();
+        }
+
+        let row = BUFFER_HEIGHT - 1;
+        let col = self.column_position;
+
+        let color_code = self.color_code;
+        self.buffer.chars[row][col].write(ScreenChar {
+          ascii_character: byte,
+          color_code,
+        });
+        self.column_position += 1;
+      }
+    }
+  }
+
+  fn new_line(&mut self) {
+    for row in 1..BUFFER_HEIGHT {
+      for col in 0..BUFFER_WIDTH {
+        let character = self.buffer.chars[row][col].read();
+        self.buffer.chars[row - 1][col].write(character);
+      }
+    }
+    self.clear_row(BUFFER_HEIGHT - 1);
+    self.column_position = 0;
+  }
+
+  fn clear_row(&mut self, row: usize) {
+    let blank = ScreenChar {
+      ascii_character: b' ',
+      color_code: self.color_code,
+    };
+    for col in 0..BUFFER_WIDTH {
+      self.buffer.chars[row][col].write(blank);
+    }
+  }
+
+  pub fn write_string(&mut self, s: &str) {
+    for byte in s.bytes() {
+      match byte {
+        // printable ASCII byte or newline
+        0x20..=0x7e | b'\n' => self.write_byte(byte),
+        // not part of printable ASCII range
+        _ => self.write_byte(0xfe),
+      }
+    }
+  }
+}
+
+impl fmt::Write for Writer {
+  fn write_str(&mut self, s: &str) -> fmt::Result {
+    self.write_string(s);
+    Ok(())
+  }
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+  use core::fmt::Write;
+  WRITER.lock().write_fmt(args).unwrap();
+}

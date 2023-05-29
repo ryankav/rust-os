e5c59384c0efbe261789e84ca45b09301fe0e94d
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-05-29 16:12:02 +0100

    Adding an exit from qemu after test

--- test_framework/src/main.rs
+++ test_framework/src/main.rs
@@ -14,6 +14,8 @@ fn test_runner(tests: &[&dyn Fn()]) {
   for test in tests {
     test();
   }
+
+  exit_qemu(QemuExitCode::Success);
 }
 
 #[panic_handler]
@@ -37,4 +39,20 @@ fn trivial_assertion() {
   print!("trivial assertion... ");
   assert_eq!(1, 1);
   println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+  Success = 0x10,
+  Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+  use x86_64::instructions::port::Port;
+
+  unsafe {
+    let mut port = Port::new(0xf4);
+    port.write(exit_code as u32);
+  }
 }

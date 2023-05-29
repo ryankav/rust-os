6e9c8009752e40322c73106d448e9f44eb029d42
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-05-29 14:54:36 +0100

    Adding println! & finishing vga blog

--- vga_buffer/src/main.rs
+++ vga_buffer/src/main.rs
@@ -6,23 +6,13 @@ use core::panic::PanicInfo;
 mod vga_buffer;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+  println!("{}", info);
   loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-  use core::fmt::Write;
-
-  vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-
-  write!(
-    vga_buffer::WRITER.lock(),
-    ", some numbers: {} {}",
-    42,
-    1.337
-  )
-  .unwrap();
-
-  loop {}
+  println!("Hello, World{}", "!");
+  panic!("Panic handler working");
 }

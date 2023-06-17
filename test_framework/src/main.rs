ce9743c310dc1f1e21ec6f1f65b403a8befedb9f
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-06-17 12:34:56 +0100

    Adding testable trait to simplify print statements

--- test_framework/src/main.rs
+++ test_framework/src/main.rs
@@ -10,10 +10,10 @@ mod serial;
 mod vga_buffer;
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Testable]) {
   serial_println!("Running {} tests", tests.len());
   for test in tests {
-    test();
+    test.run();
   }
 
   exit_qemu(QemuExitCode::Success);
@@ -47,9 +47,7 @@ pub extern "C" fn _start() -> ! {
 
 #[test_case]
 fn trivial_assertion() {
-  serial_println!("trivial assertion... ");
-  assert_eq!(0, 1);
-  println!("[ok]");
+  assert_eq!(1, 1);
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -65,5 +63,20 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
   unsafe {
     let mut port = Port::new(0xf4);
     port.write(exit_code as u32);
+  }
+}
+
+pub trait Testable {
+  fn run(&self);
+}
+
+impl<T> Testable for T
+where
+  T: Fn(),
+{
+  fn run(&self) {
+    serial_print!("{}...\t", core::any::type_name::<T>());
+    self();
+    serial_println!("[ok]");
   }
 }

35610a932a26bc5380c243e5f5fb849d7287c0db
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-07-02 18:33:43 +0100

    Adding integration tests and library

--- test_framework/tests/basic_boot.rs
+++ test_framework/tests/basic_boot.rs
@@ -1,10 +1,15 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(test_framework::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  test_framework::test_panic_handler(info)
+}
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
@@ -13,11 +18,8 @@ pub extern "C" fn _start() -> ! {
   loop {}
 }
 
-fn test_runner(tests: &[&dyn Fn()]) {
-  unimplemented!();
-}
-
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-  loop {}
+#[test_case]
+fn test_println() {
+  use test_framework::println;
+  println!("test_println output");
 }
